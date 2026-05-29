84a8b50f8741fffd4c02a1448e05fdd816b65478
Marcel <40896559+hackthedev@users.noreply.github.com>
2026-05-29 20:38:27 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -153,7 +153,7 @@ export default class dSyncInbox {
                 if (![0, 1, 2].includes(gidTableResult?.affectedRows)) {
                     Logger.warn("Messenger GID Table insert/update warning!")
                     console.log(gidTableResult)
-                    updateError = true;
+                    updateError = gidTableResult?.error ?? null;
                 }
 
                 // join own room to emit messages to
@@ -162,7 +162,7 @@ export default class dSyncInbox {
                     socket.join(userGid);
                 }
 
-                response({error: updateError ? "Couldnt set or update member info!" : null})
+                response({error: updateError ?? null})
             })
 
             socket.on("/messenger/send", async (user, response) => {
